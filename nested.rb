21b9c76775b9f704e8607d394bc062189afb9434
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-06-01 18:23:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,7 +62,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-dennis_ritchies_language = programmer_hash[:dennis_ritchie][:languages]     
+dennis_ritchies_language = programmer_hash[:dennis_ritchie][:languages]
+dennis_ritchies_language
 
 end
 
